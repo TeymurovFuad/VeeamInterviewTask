e5a49fb10216b898e03802cfc808e87f78fc31f3
teymurovfuad <fuad.teymurov@gmail.com>
2025-08-26 23:23:19 +0200

    added program runner

---
+++ VeeamTask.Runner/Program.cs
@@ -0,0 +1,44 @@
+﻿using Veeam.TaskSolution;
+using Veeam.TaskSolution.Utils;
+
+
+void StartService(string[] args)
+{
+    Console.Title = "Veeam Task Runner";
+
+    if (args.Length != 4 || !int.TryParse(args[2], out var syncIntervalInSeconds) || syncIntervalInSeconds <= 0)
+        throw new ArgumentException("Usage: VeeamTask.Runner <sourceDir> <backupDir> <syncIntervalInSeconds> <logFilePath>");
+
+    var sourceDir = args[0];
+
+    if(string.IsNullOrEmpty(sourceDir?.Trim()))
+        throw new ArgumentException("Source directory cannot be null or empty.");
+    else if (!Directory.Exists(sourceDir))
+        throw new DirectoryNotFoundException($"Source directory '{sourceDir}' does not exist.");
+
+    var backupDir = args[1];
+
+    if (string.IsNullOrEmpty(backupDir?.Trim()))
+        throw new ArgumentException("Backup directory cannot be null or empty.");
+
+    var logFilePath = args[3];
+
+    if (string.IsNullOrEmpty(logFilePath?.Trim()))
+        throw new ArgumentException("Log file path cannot be null or empty.");
+
+
+    Console.WriteLine("Starting Backup Service...");
+    Console.WriteLine("Press Ctrl+C to stop the service.");
+
+    var config = new ServiceConfig(
+               sourceDir: sourceDir,
+                backupDir: backupDir,
+                syncInterval: TimeSpan.FromSeconds(syncIntervalInSeconds),
+                logFilePath: logFilePath);
+
+    LoggerConfig.InitLogger(config.LogFilePath);
+    var fileManager = new BackupService(config);
+    fileManager.StartSync();
+}
+
+StartService(args);
